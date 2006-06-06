ae16fe46a5e795ce7363d91e4e8ed45ad874cddb
agacek <agacek@3a4acb30-9413-0410-b9df-e184e0528125>
2006-06-06 14:02:56 +0000

    Added a #show_table command.

--- trunk/bedwyr/src/table.ml
+++ trunk/bedwyr/src/table.ml
@@ -9,3 +9,8 @@ let find table args = try Some (Hashtbl.find table args) with Not_found -> None
 
 let remove table args = Hashtbl.remove table args
 
+let print table =
+  Hashtbl.iter
+    (fun args tag -> Printf.printf "%s\n"
+      (String.concat " " (List.map Pprint.term_to_string args)))
+    table
