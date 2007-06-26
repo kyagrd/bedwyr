19fcf2512829c1aaa18571f3b18ee19005c67673
dbaelde <dbaelde@3a4acb30-9413-0410-b9df-e184e0528125>
2007-06-26 21:58:04 +0000

    Fix a bug in sigmaL (use of ts instead of rhs, lead to non-sense). Add an "intuitionistic" param to the firstorder logic, and tweak a few rules depending on it in order to get muLJ instead of muLK.

--- trunk/tac/taci/Firstorder/firstorder.mli
+++ trunk/tac/taci/Firstorder/firstorder.mli
@@ -1,2 +1,4 @@
 module Firstordernonstrict : functor (O : Output.Output) -> Logic.Logic
-module Firstorderstrict :  functor (O : Output.Output) -> Logic.Logic
+module Firstorderstrict    : functor (O : Output.Output) -> Logic.Logic
+module MuLJstrict          : functor (O : Output.Output) -> Logic.Logic
+module MuLJnonstrict       : functor (O : Output.Output) -> Logic.Logic
