c3c83382990b0347e243912aaf7a378eb686a583
snow <snow@3a4acb30-9413-0410-b9df-e184e0528125>
2008-06-17 19:34:55 +0000

    More updates to READMEs, etc.

--- trunk/tac/tools/compile.bat
+++ trunk/tac/tools/compile.bat
@@ -1,4 +1,5 @@
+rem Builds tactex.exe; assumes that xml-light is installed somewhere that O'Caml can find it.
 @echo off
 echo Building tactex...
-ocamlc 	xml-light.cma str.cma tactex.ml -o ..\bin\tactex.exe
+ocamlc xml-light.cma str.cma tactex.ml -o ..\bin\tactex.exe
 echo Done.
