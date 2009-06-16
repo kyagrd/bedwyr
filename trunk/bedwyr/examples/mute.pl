b51cea063b06d7d71a578ae31a4bab0199249345
dbaelde <dbaelde@3a4acb30-9413-0410-b9df-e184e0528125>
2009-06-16 20:11:58 +0000

    Cleanup several examples. Change informal examples and tests into assertions, add files to the "make test" target. Fix mute.pl to parse pretty-printed output.

--- trunk/bedwyr/examples/mute.pl
+++ trunk/bedwyr/examples/mute.pl
@@ -1,15 +1,26 @@
 #!/usr/bin/perl
 
-$left = "" ;
+$side = "right" ;
+$content = "" ;
+# Trying to detect two consecutive printings of (left ...),
+# ignoring (left ...) following by (right ...).
+# This is slightly annoying to do because the trace (...) is pretty-printed
+# on multiple lines.
 while (<>) {
-  if (/^\(left\s+(.*)\)$/) {
-    if ("" eq $left) {
-      $left = $1 ;
+  if (/^\(([a-z]+)\s+(.*)$/) {
+    $side = $1 ;
+    if ($side eq "right") {
+      $content = "" ;
     } else {
-      print "The following trace is not doable on the right:\n$left\n" ;
-      exit 0 ;
+      if ($content ne "") {
+        chop $content ;
+        print
+          "The following trace is not doable on the right:\n      $content\n" ;
+        exit 0 ;
+      }
+      $content = $2 ;
     }
   } else {
-    $left = "" ;
+    $content .= $_ ;
   }
 }
