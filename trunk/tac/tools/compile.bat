a051dfbbb3f5844f496104c51e48d5ecb5e7b84a
snow <snow@3a4acb30-9413-0410-b9df-e184e0528125>
2010-12-03 21:47:28 +0000

    Trying to get mod2def compiling easily for Alberto.  Also some notes on the dangers of the tool.

--- trunk/tac/tools/compile.bat
+++ trunk/tac/tools/compile.bat
@@ -1,6 +1,6 @@
 rem Builds tactex.exe and mod2def.exe; assumes that xml-light is installed somewhere that O'Caml can find it.
 @echo off
 echo Building tools...
-ocamake xml-light.cmxa str.cmxa tactex.ml -opt -epp -o ..\bin\tactex.exe
-ocamake mod2def.ml ..\taci\output.ml* ..\taci\listutils.ml* ..\taci\Firstorder\LP\*.ml* -opt -epp -o ..\bin\mod2def.exe
+ocamake xml-light.cma str.cma tactex.ml -epp -o ..\bin\tactex.exe
+ocamake mod2def.ml str.cma ..\taci\properties.ml* ..\taci\option.ml* ..\taci\output.ml* ..\taci\listutils.ml* ..\taci\Firstorder\LP\*.ml* -epp -o ..\bin\mod2def.exe
 echo Done.
