7a268a57c574bf84fc2f1328dd2c53f64c209f79
tiu <atiu@ntu.edu.sg>
2012-07-16 06:11:08 +0000

    Created a branch for polymorphic typing. This is just a temporary repository. Will be merged into the trunk once it's stable.

--- tac/taci/clean.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Cleaning...
-ocamake -clean *.ml* Propositional/*.ml* Nologic/*.ml* Firstorder/*.ml* Firstorder/LP/*.ml* ../../bedwyr/src/ndcore/*.ml*
-ocamake -clean *.ml* Propositional/*.ml* Nologic/*.ml* Firstorder/*.ml* Firstorder/LP/*.ml* ../../bedwyr/src/ndcore/*.ml* -opt
-echo Done.
--- tac/tools/clean.bat
+++
@@ -1,6 +0,0 @@
-rem Deletes intermediate files.
-@echo offf
-echo Cleaning...
-ocamake xml-light.cmxa str.cmxa tactex.ml -opt -epp -o ..\bin\tactex.exe -clean
-ocamake mod2def.ml ..\taci\output.ml* ..\taci\listutils.ml* ..\taci\Firstorder\LP\*.ml* -opt -epp -o ..\bin\mod2def.exe -clean
-echo Done.
