e6e0b24e21fa6e81ccebf853f4574656f6f5defc
heath <heath@3a4acb30-9413-0410-b9df-e184e0528125>
2015-05-28 11:35:05 +0000

    v1.4-beta6

---
+++ trunk/bedwyr/src/prover/config.mli
@@ -0,0 +1,27 @@
+(****************************************************************************)
+(* Bedwyr prover                                                            *)
+(* Copyright (C) 2011-2015 Quentin Heath                                    *)
+(*                                                                          *)
+(* This program is free software; you can redistribute it and/or modify     *)
+(* it under the terms of the GNU General Public License as published by     *)
+(* the Free Software Foundation; either version 2 of the License, or        *)
+(* (at your option) any later version.                                      *)
+(*                                                                          *)
+(* This program is distributed in the hope that it will be useful,          *)
+(* but WITHOUT ANY WARRANTY; without even the implied warranty of           *)
+(* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            *)
+(* GNU General Public License for more details.                             *)
+(*                                                                          *)
+(* You should have received a copy of the GNU General Public License along  *)
+(* with this program; if not, write to the Free Software Foundation, Inc.,  *)
+(* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.              *)
+(****************************************************************************)
+
+(** Build-time global variables. *)
+
+val package_name : string
+val package_version : string
+val ocaml_version : string
+val build : string
+val build_date : string
+val features : (string * string) list
