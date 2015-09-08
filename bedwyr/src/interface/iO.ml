a6b9ad00ece477a278909f08df0beaf4d23d4d79
heath <quentin.heath@lix.polytechnique.fr>
2015-09-08 10:11:25 +0000

    code: large but shallow reorganization (move things towards/within the libs)
    mostly functions moved from Bedwyr to System, IO, Environment and
    Interface, and bugfixes in these functions (pertaining to environment
    manipulation, file input, etc.)
    
    this improves on the "file backtracking" from 2 commits ago, by
    selectively cancelling the last definition *files*

--- bedwyr/src/interface/iO.ml
+++ bedwyr/src/interface/iO.ml
@@ -21,69 +21,128 @@
 exception File_error of string * string * string
 
 
-(* Sanity wrappers *)
-
 let error name e s =
   let msg = Str.replace_first (Str.regexp (Str.quote (name ^ ": "))) "" e in
   raise (File_error (s,name,msg))
 
-let open_in name =
-  try open_in name
-  with Sys_error e -> error name e "read from"
+(* Sanity wrappers *)
 
-let close_in name c =
-  try close_in c
-  with Sys_error e -> error name e "close"
-
-let run_in f name =
-  let c = open_in name in
-  let result =
-    try f c with e ->
-      close_in name c ;
-      raise e
-  in
-  close_in name c ;
-  result
-
-let open_out name =
-  try open_out_gen [Open_wronly;Open_creat;Open_excl] 0o600 name
-  with Sys_error e -> error name e "create"
-
-let close_out name c =
-  try close_out c
-  with Sys_error e -> error name e "close"
-
-let run_out f name =
-  let c = open_out name in
-  let result =
-    try f c with e ->
-      close_out name c ;
-      raise e
-  in
-  close_out name c ;
-  result
+let paths_are_relative = ref true
+let bwd = ref ""
+let chrooted = ref false
 
 let chdir name =
   try Sys.chdir name
   with Sys_error e -> error name e "chdir in"
 
-(* List of open files used for user I/O. *)
+(* [run_on f path] is supposedly functionally equivalent to
+ * [f basename path path], except for additional checks on the path.
+ * [f ~basename ~nice_path ~full_path] is expected to use [basename] for
+ * access, [nice_path] for error messages and [full_path] for unique
+ * identification. *)
+let run_on f path =
+  let clean =
+    let old_cwd = Sys.getcwd ()
+    and paths_were_relative = !paths_are_relative in
+    function () ->
+      paths_are_relative := paths_were_relative ;
+      chdir old_cwd
+  in
+  let result = try
+    let dirname = Filename.dirname path in
+    chdir dirname ;
+    if not (Filename.is_relative dirname) then (paths_are_relative := false) ;
+    let cwd = Sys.getcwd ()
+    and basename = Filename.basename path in
+    let full_path = Filename.concat cwd basename in
+    let nice_path =
+      if !paths_are_relative
+      then Str.replace_first (Str.regexp ("^" ^ !bwd ^ "/")) "" full_path
+      else full_path
+    in
+    if !chrooted && nice_path=full_path
+    (* XXX we use path instead of nice_path to avoid leaking bwd
+     * although technically, by trying to access "../../aa/bb", one
+     * can still discover this path *)
+    then error path "Outside of the authorized directory" "access"
+    else f ~basename ~nice_path ~full_path
+  with e ->
+    clean () ;
+    raise e
+  in
+  clean () ;
+  result
+
+let open_in ~nice_path name =
+  try open_in name
+  with Sys_error e -> error nice_path e "read from"
+
+let close_in ~nice_path c =
+  try close_in c
+  with Sys_error e -> error nice_path e "close"
+
+let run_in ~wrap (f : (?fname:string -> in_channel -> 'a)) name =
+  let aux ~basename ~nice_path ~full_path =
+    let c = open_in ~nice_path basename in
+    let clean () = close_in ~nice_path c in
+    let result =
+      try f ~fname:nice_path c
+      with e -> clean () ; raise e
+    in
+    clean () ;
+    result
+  in
+  run_on (wrap aux) name
+
+let get_in fname =
+  run_in ~wrap:(function f -> f)
+    (fun ?fname:_ c -> really_input_string c (in_channel_length c))
+    fname
+
+let open_out ~nice_path name =
+  try open_out_gen [Open_wronly;Open_creat;Open_excl] 0o600 name
+  with Sys_error e -> error nice_path e "create"
+
+let close_out ~nice_path c =
+  try close_out c
+  with Sys_error e -> error nice_path e "close"
+
+let run_out f name =
+  let aux ~basename ~nice_path ~full_path =
+    let c = open_out ~nice_path basename in
+    let clean () = close_out ~nice_path c in
+    let result = try f c with e -> clean () ; raise e in
+    clean () ;
+    result
+  in
+  run_on aux name
+
+(* List of open files used for user predicate I/O. *)
 
 module I = struct
   let files : (string,(in_channel * Lexing.lexbuf)) Hashtbl.t = Hashtbl.create 50
 
   let get name =
-    try Some (Hashtbl.find files name)
-    with Not_found -> None
+    let aux ~basename ~nice_path ~full_path =
+      try Some (Hashtbl.find files full_path)
+      with Not_found -> None
+    in
+    run_on aux name
 
   let add name =
-    let c = open_in name in
-    let l = Lexing.from_channel c in
-    Hashtbl.replace files name (c,l)
+    let aux ~basename ~nice_path ~full_path =
+      let c = open_in ~nice_path basename in
+      let l = Lexing.from_channel c in
+      Hashtbl.replace files full_path (c,l)
+    in
+    run_on aux name
 
   let remove name c =
-    close_in name c ;
-    Hashtbl.remove files name
+    let aux ~basename ~nice_path ~full_path =
+      close_in ~nice_path c ;
+      Hashtbl.remove files full_path
+    in
+    run_on aux name
 
   let clear () =
     Hashtbl.iter
@@ -96,17 +155,26 @@ module O = struct
   let files : (string,(out_channel * Format.formatter)) Hashtbl.t = Hashtbl.create 50
 
   let get name =
-    try Some (Hashtbl.find files name)
-    with Not_found -> None
+    let aux ~basename ~nice_path ~full_path =
+      try Some (Hashtbl.find files full_path)
+      with Not_found -> None
+    in
+    run_on aux name
 
   let add name =
-    let c = open_out name in
-    let f = Format.formatter_of_out_channel c in
-    Hashtbl.replace files name (c,f)
+    let aux ~basename ~nice_path ~full_path =
+      let c = open_out ~nice_path basename in
+      let f = Format.formatter_of_out_channel c in
+      Hashtbl.replace files full_path (c,f)
+    in
+    run_on aux name
 
   let remove name c =
-    close_out name c ;
-    Hashtbl.remove files name
+    let aux ~basename ~nice_path ~full_path =
+      close_out ~nice_path c ;
+      Hashtbl.remove files full_path
+    in
+    run_on aux name
 
   let clear () =
     Hashtbl.iter
