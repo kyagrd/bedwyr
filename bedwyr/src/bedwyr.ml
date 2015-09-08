2812d27253a2df27da05c696b035d373d1c85850
heath <quentin.heath@lix.polytechnique.fr>
2015-09-08 10:11:49 +0000

    code: adapt to the previous API changes

--- bedwyr/src/bedwyr.ml
+++ bedwyr/src/bedwyr.ml
@@ -27,41 +27,69 @@ let () =
     with Failure ("int_of_string") | Not_found -> 72
   in
   List.iter
-    (fun fref -> Output.set_width !fref term_width)
-    [Output.std_out;Output.std_err;Output.std_dbg]
+    (fun fref -> IO.Output.set_width !fref term_width)
+    [IO.Output.std_out;IO.Output.std_err;IO.Output.std_dbg]
 
 let welcome_msg =
+  let open Interface.Config in
   Printf.sprintf
     "%s %s%s welcomes you.\n\
     \n\
     For a little help, type \"#help.\"\n"
-    Config.package_name
-    Config.package_version
-    (if Config.build="v"^Config.package_version || Config.build="" then ""
-     else " (revision " ^ Config.build ^ ")")
+    package_name
+    package_version
+    (if build="v"^package_version || build="" then ""
+     else " (revision " ^ build ^ ")")
+
+let plugins = ref []
+
+let add_plugin_dir,add_plugin =
+  let plugin_dirs =
+    ref [
+      Findlib.package_directory Interface.Config.project_tarname ;
+    ]
+  in
+  let add_plugin_dir dir = plugin_dirs := dir :: !plugin_dirs
+  and add_plugin (plugin,desc) =
+    let open Dynlink in
+    let rec aux = function
+      | [] -> ()
+      | dir::dirs ->
+          try
+            loadfile (adapt_filename (Filename.concat dir plugin ^ ".cma")) ;
+            plugins := desc :: !plugins
+          with Error e -> aux dirs
+    in
+    aux !plugin_dirs
+  in
+  add_plugin_dir,add_plugin
+
+let () =
+  List.iter add_plugin [
+    "tableXmlExport","XML export of proofs" ;
+  ]
 
 let version_msg =
+  let open Interface.Config in
   Printf.sprintf
     "%s prover %s, Copyright (C) 2005-2015 Slimmer project.\n\
     This is free software, distributed under the GNU General Public License\n\
     version 2.  There is NO WARRANTY, not even SOUNDNESS nor COMPLETENESS.\n\
-    %s (built with OCaml %s on the %s).\n\
-    Features (+/-):%s\n"
-    Config.package_name
-    Config.package_version
-    (if Config.build="" then "Unknown revision"
-     else "Rev. " ^ Config.build ^ "")
-    Config.ocaml_version
-    Config.build_date
-    (String.concat ""
-       (List.flatten
-          (List.map
-             (fun (s1,s2) ->
-                [(match s2 with "" -> "\n - " | _ -> "\n + ") ; s1])
-             Config.features)))
+    Rev. %s (built with OCaml %s on the %s).\n\
+    Features (+/-):%s\n\
+    Plugins (+/-):%s\n"
+    package_name
+    package_version
+    (if build="" then "unknown" else build)
+    ocaml_version
+    build_date
+    (let get (s1,s2) = [(match s2 with "" -> "\n - " | _ -> "\n + ") ; s1] in
+     String.concat "" (List.flatten (List.map get features)))
+    (let get s = [("\n + ") ; s] in
+     String.concat "" (List.flatten (List.map get !plugins)))
 
 let print_version () =
-  Output.printf ~nl:true "%s" version_msg ;
+  IO.Output.printf ~nl:true "%s" version_msg ;
   exit 0
 
 let usage_msg =
@@ -71,51 +99,52 @@ let usage_msg =
 let batch       = ref false
 
 (* Bedwyr's working directory *)
-let () = IO.bwd := Sys.getcwd ()
+let () = IO.Files.bwd := Sys.getcwd ()
 module InclFiles =
   Set.Make (struct type t = string let compare = compare end)
 let inclfiles = ref (InclFiles.empty)
 
 let _ =
-  Arg.parse
-    (Arg.align
-       [ "-I", Arg.Set batch,
-           " Do not enter interactive mode" ;
-         "-t", Arg.Unit Interface.incr_test_limit,
-           " Run tests (use as many times as the #include-depth)" ;
-         "-T", Arg.Unit Interface.remove_test_limit,
-           " Run tests (use as many times as the #include-depth)" ;
-         "--filter", Arg.Set System.use_filter,
-           "Use tabling with variables" ;
-         "-d", Arg.String (fun s -> Interface.definitions := s::!Interface.definitions),
-           "<s> Add definition" ;
-         "-e", Arg.String (fun s -> Interface.queries := s::!Interface.queries),
-           "<s> Execute query" ;
-         "--freezing", Arg.Set_int Prover.freezing_point,
-           "<n> Enable backward chaining and set its limit" ;
-         "--saturation", Arg.Set_int Prover.saturation_pressure,
-           "<n> Enable forward chaining and set its limit" ;
-         "--version", Arg.Unit print_version,
-           " Display version info and exit" ;
-         "-D", Arg.Set Output.debug,
-           " Print debugging information" ;
-         "--colours", Arg.Int (fun n -> Output.set_colours n),
-          " Number of supported colours (256 or 8; 0 do disable)" ;
-       ])
-    (fun f -> Interface.session := f::!Interface.session)
-    usage_msg ;
-  Interface.session := List.rev (!Interface.session) ;
-  Interface.definitions := List.rev (!Interface.definitions) ;
-  Interface.queries := List.rev (!Interface.queries)
+  Prover.System.(
+    Arg.parse
+      (Arg.align
+         [ "-I", Arg.Set batch,
+             " Do not enter interactive mode" ;
+           "-t", Arg.Unit incr_test_limit,
+             " Run tests (use as many times as the #include-depth)" ;
+           "-T", Arg.Unit remove_test_limit,
+             " Run tests (use as many times as the #include-depth)" ;
+           "--filter", Arg.Set use_filter,
+             "Use tabling with variables" ;
+           "-d", Arg.String (fun s -> definitions := s::!definitions),
+             "<s> Add definition" ;
+           "-e", Arg.String (fun s -> queries := s::!queries),
+             "<s> Execute query" ;
+           "--freezing", Arg.Set_int Prover.Proofsearch.freezing_point,
+             "<n> Enable backward chaining and set its limit" ;
+           "--saturation", Arg.Set_int Prover.Proofsearch.saturation_pressure,
+             "<n> Enable forward chaining and set its limit" ;
+           "--version", Arg.Unit print_version,
+             " Display version info and exit" ;
+           "-D", Arg.Set IO.Output.debug,
+             " Print debugging information" ;
+           "--colours", Arg.Int (fun n -> IO.Output.set_colours n),
+            "<n> Number of supported colours (256 or 8; 0 do disable)" ;
+         ])
+      (fun f -> session := f::!session)
+      usage_msg ;
+    session := List.rev (!session) ;
+    definitions := List.rev (!definitions) ;
+    queries := List.rev (!queries))
 
 let _ =
-  Interface.reload () ;
-  Interface.Status.exit_if () ;
+  Interface.Run.reload () ;
+  Prover.System.Status.exit_if () ;
   List.iter
-    (function query -> ignore (Interface.run_query_string query))
-    !Interface.queries ;
-  if !batch then Interface.Status.exit_with ()
+    (function query -> ignore (Interface.Run.query_string query))
+    !Prover.System.queries ;
+  if !batch then Prover.System.Status.exit_with ()
   else begin
-    Output.printf ~nl:true "%s" welcome_msg ;
-    Interface.run_queries_channel stdin
+    IO.Output.printf ~nl:true "%s" welcome_msg ;
+    Interface.Run.queries_channel stdin
   end
